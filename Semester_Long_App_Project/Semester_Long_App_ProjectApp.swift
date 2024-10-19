475d21e676dc8bdc18244118afb7ba0296955fb7
Keean <sctheswordbearer@gmail.com>
2024-10-18 19:04:09 -0500

    Initial Commit

---
+++ Semester_Long_App_Project/Semester_Long_App_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Semester_Long_App_ProjectApp.swift
+//  Semester_Long_App_Project
+//
+//  Created by Shield on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Semester_Long_App_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
